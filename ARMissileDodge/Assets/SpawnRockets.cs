05a8d27ba8149c3a2e18ae1472a4b5d6d4167bc7
Nipun Chhajed <nipunchhajed@gmail.com>
2017-11-12 13:09:38 -0800

    unfinished project

--- ARMissileDodge/Assets/SpawnRockets.cs
+++ ARMissileDodge/Assets/SpawnRockets.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnRockets : MonoBehaviour {
-	public GameObject[] enemy;
-	public Vector3 spawnValues;
+	public GameObject enemy;
+	public List<GameObject> enemies = new List<GameObject>();
+	public int score;
 	public float spawnWait;
 	public float spawnMostWait;
 	public float spawnLeastWait;
 	public int startWait;
 	public bool stop;
+	public Time time;
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (Spawner ());
@@ -21,13 +23,28 @@ public class SpawnRockets : MonoBehaviour {
 	}
 	IEnumerator Spawner(){
 		yield return new WaitForSeconds (startWait);
-		while (!stop) {
-			int randEnemy = Random.Range (0, 4);
-			Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x,spawnValues.x),5,Random.Range(-spawnValues.z,spawnValues.z));
-
-			Instantiate (enemy[randEnemy], spawnPosition + transform.TransformPoint (0, 0, 0), gameObject.transform.rotation);
-
+		Vector3 spawnRotation = new Vector3 (90, 0, 0);
+		while (PlayerController.dead != true) { 
+			score = 0;
+			Vector3 spawnPosition = new Vector3 ((float)Random.Range (-1,1), 4 ,(float)(Random.Range(-1,1)));
+			Instantiate (enemy, spawnPosition,  Quaternion.Euler(spawnRotation));
+			enemies.Add(enemy);
+			/*for (int i = 0; i <= enemies.Count; i++) {
+				if (enemies[i].transform.position.y <= 0) {
+					Destroy(gameObject);
+					enemies.Remove (enemies [i]);
+					Instantiate (enemy, spawnPosition,  Quaternion.Euler(spawnRotation));
+					score += 1;
+				}
+					
+			}*/
 			yield return new WaitForSeconds (spawnWait);
+		}
+	}
+	void OnCollisionEnter(Collision col) {
+		if(col.gameObject.name == "trump_Ip_anim_iddle01") {
+			Destroy (col.gameObject);
+			PlayerController.dead = true;
 		}
 	}
 }
